9ec6f43a99777fdec6f0ac2ae32a363d55978e7f
Jagratihooda <jagratigit@gmail.com>
2017-01-22 01:23:28 +0530

    test

--- src/main/java/com/padmesh/product/dto/Product1.java
+++ src/main/java/com/padmesh/product/dto/Product1.java
@@ -1,20 +1,8 @@
 package com.padmesh.product.dto;
 
 public class Product1 {
-	private int ING_id;
+	private int id;
 	private String ING_name;
-	public int getING_id() {
-		return ING_id;
-	}
-	public void setING_id(int iNG_id) {
-		ING_id = iNG_id;
-	}
-	public String getING_name() {
-		return ING_name;
-	}
-	public void setING_name(String iNG_name) {
-		ING_name = iNG_name;
-	}
 	
 		
 

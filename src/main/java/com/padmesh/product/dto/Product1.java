30ac75980d85f92b6ed5c1b797873340ddda9a72
root <root@localhost.localdomain>
2017-03-10 17:29:37 +0530

    test

--- src/main/java/com/padmesh/product/dto/Product1.java
+++ src/main/java/com/padmesh/product/dto/Product1.java
@@ -5,7 +5,7 @@ public class Product1 {
 	private int ING_Id;
 	private String ING_First_Name;
 	private String ING_Last_Name;
-	private int ING_Age;
+	private int Age_test;
 	private String ING_Address; 
 	private String ING_Spouse_Name;
 	private String ING_Gender;

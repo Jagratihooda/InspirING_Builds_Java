4daeaca9dedb597aa2e7c7e2f62ef8196cdbe0aa
Jagratihooda <jagratigit@gmail.com>
2017-04-17 18:26:10 +0530

    removed ING_ prefix from Address variable

---
+++ Product1.java
@@ -0,0 +1,14 @@
+package com.padmesh.product.dto;
+
+public class Product1 {
+	
+	private int ING_Id;
+	private String ING_First_Name;
+	private String ING_Last_Name;
+	private int Age;
+	private String Address; 
+	private String ING_Spouse_Name;
+	private String ING_Gender;
+	private String ING_Mobile_Number;
+
+}
